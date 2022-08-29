42904115126243f1a0aa002daf51209936b6838f
JS_Ninja <km01286486191@gmail.com>
2022-08-30 01:46:27 +0200

    add sanity to project

---
+++ components/FooterPanel/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FooterPanel() {
+  return <div>FooterPanel</div>;
+}
+
+export default FooterPanel;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
 import React from "react";
 
-const Home = () => {
+import Products from "../components/Products";
+import FooterPanel from "../components/FooterPanel";
+import HeroPanel from "../components/HeroPanel";
+
+import { client } from "../lib/client";
+
+const Home = ({ products, bannerDate }) => {
   return (
     <>
-      Header
-      <div>
+      <HeroPanel />
+      <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speakers Of Many Variations</p>
       </div>
-      <div>{["product1", "product2"].map((product) => product)}</div>
-      footer
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterPanel />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerDate = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerDate },
+  };
 };
 
 export default Home;
